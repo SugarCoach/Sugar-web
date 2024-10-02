488a67b4dd3f0b7c776dcbb7eba0a393a9f73898
luciocape <luciodimascapezzuto@gmail.com>
2024-10-02 10:04:07 -0300

    Agregue variables de entorno al front (VUE_APP_API_URL)

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.VUE_APP_API_URL,
   // Aquí puedes añadir más configuraciones como headers por defecto
 });
 
