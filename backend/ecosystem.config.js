3853c9d6d43dbca659c22e92667873609c06ee8a
luciocape <luciodimascapezzuto@gmail.com>
2024-10-03 09:35:14 -0300

    cambio de prueba

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
         name   : "sugar-web-api",
-        script : "./index.js",
+        script : "index.js",
         watch : true,
         max_memory_restart: '1000M'
     }]
