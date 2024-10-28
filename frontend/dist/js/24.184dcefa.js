0a5aa0fec46bd78b7a1d294885d6d2081f3cc284
luciocape <luciodimascapezzuto@gmail.com>
2024-10-28 12:29:07 -0300

    Cambie el .gitignore para que no ignore el dist y si ignore el src y public

---
+++ frontend/dist/js/24.184dcefa.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkweb"]=self["webpackChunkweb"]||[]).push([[24],{9024:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var u=n(6768),c={__name:"Deleted",setup(e){return setTimeout((function(){window.location.replace("/")}),2e3),(e,t)=>{const n=(0,u.g2)("q-page");return(0,u.uX)(),(0,u.Wv)(n,{class:""},{default:(0,u.k6)((()=>t[0]||(t[0]=[(0,u.Lk)("h1",{class:"text-center"},"Su cuenta se borro con exito",-1)]))),_:1})}}};const r=c;var s=r}}]);
+//# sourceMappingURL=24.184dcefa.js.map
---
+++ frontend/dist/js/809.e46306d1.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkweb"]=self["webpackChunkweb"]||[]).push([[809],{4809:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=n(6768);function s(e,t,n,s,r,c){const a=(0,u.g2)("q-page");return(0,u.uX)(),(0,u.Wv)(a,null,{default:(0,u.k6)((()=>t[0]||(t[0]=[(0,u.Lk)("div",{class:"d-flex justify-content-center align-items-center",style:{height:"100vh",width:"100%"}},[(0,u.Lk)("h1",{style:{"font-size":"20vw"}},"404")],-1)]))),_:1})}var r={name:"NotFound"},c=n(1241);const a=(0,c.A)(r,[["render",s]]);var i=a}}]);
+//# sourceMappingURL=809.e46306d1.js.map
