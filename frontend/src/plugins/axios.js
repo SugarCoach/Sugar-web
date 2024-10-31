4aad7fb2e88562ec7f6f57a97c4828fc895bec11
luciocape <luciodimascapezzuto@gmail.com>
2024-10-31 12:25:11 -0300

    Elimino src y public del rastreo

--- frontend/src/plugins/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
-  // Aquí puedes añadir más configuraciones como headers por defecto
-});
-
-export default axiosInstance;
