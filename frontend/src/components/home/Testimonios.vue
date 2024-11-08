7f1cbaad4cad1655853e4e6b43492c7440f27755
luciocape <luciodimascapezzuto@gmail.com>
2024-11-08 19:25:17 -0300

    Añado la sección Testimonios

---
+++ frontend/src/components/home/Testimonios.vue
@@ -0,0 +1,79 @@
+<template>
+    <section id="testimonios">
+        <h2 class="m-auto m-0 mb-5">Testimonios</h2>
+        <div class="position-relative testimonios-container">
+            <div
+                class="arrow left position-absolute m-3 m-sm-5 rounded-circle d-flex justify-content-center align-items-center">
+                <iconArrow class="icon" />
+            </div>
+            <testimonio class="t1" user="Pablo" opinion="Muy buena aplicación" :stars="3" size="md" />
+            <testimonio class="t2" user="Pablo" opinion="Muy buena aplicación" :stars="4" size="md" />
+            <testimonio class="m-auto" user="Pablo" opinion="Muy buena aplicación" :stars="5" size="lg" />
+
+            <div
+                class="arrow right position-absolute m-3 m-sm-5 rounded-circle d-flex justify-content-center align-items-center">
+                <iconArrow class="icon" />
+            </div>
+        </div>
+        <div class="d-flex justify-content-center gap-5 align-items-center mt-5">
+            <input v-for="index in 6" :key="index"type="radio" :checked="index == 1 ? true:false" name="select-testimonio">
+        </div>
+    </section>
+</template>
+
+<script setup>
+import iconArrow from '../reutilizable/icons/_arrow.vue';
+import testimonio from '../reutilizable/testimonio.vue';
+
+name: 'testimonios';
+components: [testimonio, iconArrow]
+</script>
+
+<style scoped lang="scss">
+#testimonios {
+    h2 {
+        width: 64%;
+    }
+    input{
+        transform: scale(1.7);
+        accent-color: #b23abb;      
+    }
+
+    .testimonios-container {
+        vertical-align: middle;
+        .t1 {
+            top: 20%;
+            left: 18vw;
+        }
+
+        .t2 {
+            top: 20%;
+            right: 18vw;
+        }
+
+        .arrow {
+            min-width: 35px;
+            width: 6vw;
+            max-width: 50px;
+            aspect-ratio: 1/1;
+            background: linear-gradient(30deg, #ffffff 0%, #b23abbf3 64%);
+            top: 45%;
+
+            &.left {
+                left: 0;
+            }
+
+            &.right {
+                right: 0;
+                transform: rotate(180deg);
+            }
+
+            .icon {
+                filter: drop-shadow(-2px -1px 3px #ffffffbe) drop-shadow(2px 1px 4px #00000083);
+            }
+        }
+    }
+
+
+}
+</style>
---
+++ frontend/src/components/reutilizable/testimonio.vue
@@ -0,0 +1,77 @@
+<template>
+    <article
+        class="testimonio d-flex flex-column align-items-center justify-content-between gap-2 rounded-3 border border-2 border-white" :class="props.size">
+        <header class="text-center d-flex flex-column justify-content-center flex-fill">
+            <h3 class="m-0">{{ props.user }}</h3>
+            <p class="fw-light m-0">"{{ props.opinion }}"</p>
+        </header>
+
+        <div class="d-flex gap-3 h-25">
+            <iconStar v-for="index in 5" :key="index" :size="props.size == 'lg' ? '30px' : '20px'" :fill="index <= stars ? true : false" />
+        </div>
+    </article>
+</template>
+
+<script setup>
+import iconStar from './icons/_star.vue';
+
+name: 'testimonio';
+components: [iconStar]
+const props = defineProps({
+    user: {
+        type: String,
+        required: true,
+    },
+    opinion: {
+        type: String,
+        required: true,
+    },
+    stars: {
+        type: Number,
+        required: true,
+    },
+    size: {
+        type: String,
+        default: "lg",
+    }
+
+});
+</script>
+
+<style scoped lang="scss">
+$lg: 40%;
+$md: 20%;
+
+.testimonio {
+    background: linear-gradient(30deg, #ffffff 0%, #b23abbf3 64%);
+
+    aspect-ratio: 6/5;
+    box-shadow: 1px 6px 25px -5px rgba(0, 0, 0, 0.3);
+    &.lg{
+        min-width: 250px;
+        max-width: 350px;
+        width: $lg;
+        z-index: 40;
+        position: relative;
+        h3{
+            font-size: 1.2rem;
+        }
+        p{
+            font-size: 1.7rem;
+        }
+    }
+    &.md{
+        min-width: 200px;
+        max-width: 250px;
+        width: $md;
+        z-index: 30;
+        position: absolute;
+        h3{
+            font-size: 1rem;
+        }
+        p{
+            font-size: 1.3rem;
+        }
+    }
+}
+</style>
