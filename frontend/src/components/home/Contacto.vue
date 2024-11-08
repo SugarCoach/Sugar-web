53f32b4df1c5437fc0842d8a84ed8751bdcd1b6b
luciocape <luciodimascapezzuto@gmail.com>
2024-11-08 19:45:17 -0300

    Añado la sección de contacto

---
+++ frontend/src/components/home/Contacto.vue
@@ -0,0 +1,54 @@
+<template>
+    <section id="contacto" class="text-white position-relative d-flex">
+        <div class="filtro"></div>
+        <header class="w-50 ms-5 p-4">
+            <h2 class="text-start mb-4">Contactanos</h2>
+            <p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque atque, quibusdam quia, alias illum
+                distinctio libero tempore maxime eos enim voluptas eum temporibus totam incidunt fuga consequatur quis
+                architecto repudiandae?</p>
+            <ctaBtn class="">Sobre nosotros</ctaBtn>
+        </header>
+        <formulario class="position-absolute form"/>
+    </section>
+</template>
+
+<script setup>
+import ctaBtn from '../reutilizable/ctaBtn.vue';
+import formulario from '../reutilizable/formulario.vue';
+
+name: "contacto";
+components: [ctaBtn, formulario]
+</script>
+
+<style scoped lang="scss">
+#contacto {
+    height: 80vh;
+    min-height: fit-content;
+    background-image: url("../../../public/contenido/nene-usando-app.jpg");
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    .form{
+        z-index: 65;
+        right: 7%;
+        top: 30%;
+        max-width: 50%;
+    }
+    header{
+        position: relative;
+        z-index: 60;
+        p{
+            margin-top: 5%;
+            width: 90%;
+        }
+    }
+    .filtro{
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.6);
+        position: absolute;
+        top: 0;
+        z-index: 40;
+    }
+}
+</style>
---
+++ frontend/src/components/reutilizable/formulario.vue
@@ -0,0 +1,69 @@
+<template>
+    <form action="" class="d-flex flex-column gap-5 px-4 pb-5 pt-4 rounded-4">
+        <div class="d-flex flex-column gap-4">
+            <div v-for="item in inputs" :key="item.id" class="d-flex flex-column">
+                <label :for="item.for" class="text-black mb-1 lead fw-normal">{{ item.label }}</label>
+                <input v-if="item.type != 'textarea' && item.type != 'submit'" class="form-control message"
+                    :name="item.for" :type="item.type" :placeholder="item.placeholder">
+                <textarea v-else :name="item.for" id="" :placeholder="item.placeholder"
+                    class="form-control message"></textarea>
+            </div>
+        </div>
+        <input type="submit" value="Enviar" class="border-0 bg-dark enviar p-2 m-auto my-0 rounded-2">
+    </form>
+</template>
+
+<script setup>
+name: 'formulario';
+const inputs = [
+    {
+        type: 'text',
+        label: 'Nombre',
+        for: 'name',
+        placeholder: 'Juan Perez',
+        id: 0
+    },
+    {
+        type: 'email',
+        label: 'Email',
+        for: 'email',
+        placeholder: 'juanperez@gmail.com',
+        id: 1
+    },
+    {
+        type: 'textarea',
+        label: 'Mensaje',
+        for: 'mensaje',
+        placeholder: 'Hola, queria hacer una consulta acerca de...',
+        id: 2
+    },
+]
+</script>
+
+<style scoped lang="scss">
+form {
+    background-color: #F5F5F5;
+    width: 450px;
+    box-shadow: 1px 10px 20px -8px rgba(0, 0, 0, 0.3);
+    textarea {
+        height: 100px;
+        resize: none
+    }
+
+    input {
+        height: 45px;
+    }
+
+    .message {
+        border: 1px solid rgba(0, 0, 0, 0.4);
+        background-color: #FEFEFE;
+    }
+
+    .enviar {
+        background-color: #686868;
+        color: white;
+        max-width: 60%;
+        width: 200px;
+    }
+}
+</style>
