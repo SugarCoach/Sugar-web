c60b9f88c41f5921ac822b647eb36e247f7b6954
luciocape <luciodimascapezzuto@gmail.com>
2024-11-08 19:44:12 -0300

    Añado el componente textYLink

---
+++ frontend/src/components/reutilizable/textYLink.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="d-flex flex-column align-items-center gap-2" :style="textColor">
+        <h4 class="h3 text-center text-black">{{ props.text }}</h4>
+        <slot></slot>
+    </div>
+
+</template>
+
+<script setup>
+name: 'textLink';
+const props = defineProps({
+    text:{
+        type: String,
+        required: true,
+    },
+    color:{
+        type: String,
+        default: '#000000;',
+    }
+});
+const textColor = `color: ${props.color};`;
+</script>
+
+<style scoped lang="scss">
+</style>
