7b4ff0786bb5275b145aaf533f4b506de2f4cff6
luciocape <luciodimascapezzuto@gmail.com>
2024-11-08 19:46:28 -0300

    Añado el componente de descargarApp

--- frontend/src/components/reutilizable/descargarApp.vue
+++ frontend/src/components/reutilizable/descargarApp.vue
@@ -1,9 +1,19 @@
 <template>
-	<a href=""><img src="../../../public/contenido/boton-descargar.webp" alt="Descargar Google Play" width="150px"></a>
+	<a href=""><img src="../../../public/contenido/boton-descargar.webp" alt="Descargar Google Play" :width="props.size"></a>
 </template>
 
 <script setup>
 name: "descargarApp";
+const props = defineProps({
+    size: {
+        type: String,
+        default: "150px"
+    },
+});
 </script>
 
-<style></style>
+<style scoped lang="scss">
+a{
+	z-index: 80;
+}
+</style>
