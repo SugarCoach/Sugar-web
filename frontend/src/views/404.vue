4fd86d44bb6564fafff6ef375638173ffaa22e19
luciocape <luciodimascapezzuto@gmail.com>
2025-06-01 19:05:26 -0300

    build, hice el build y dejo de rastrear todos los archivos/carpetas que no necesite

--- frontend/src/views/404.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <main>
-        <div class="d-flex justify-content-center align-items-center" style="height: 100vh; width: 100%"><h1 style="font-size: 20vw">404</h1></div>
-    </main>
-</template>
-
-<script setup>
-    name: 'NotFound'
-</script>
